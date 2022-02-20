5a2a2d1ddd8cda0c1048c3ac47b54fce9db6ddd9
gabrielccsantos42 <gabrielccsantos42@gmail.com>
2022-02-20 16:38:23 -0300

    Organização na classe Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,12 +6,17 @@ import java.util.Scanner;
 import entities.Cliente;
 import entities.Conta;
 import entities.ContaCorrente;
+import entities.ContaPoupanca;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner read = new Scanner(System.in);
+		
+		Cliente cliente;
+		ContaCorrente acc;
+		ContaPoupanca acp;
 		
 		int conta, numero;
 		double deposito, retirada;
@@ -26,7 +31,7 @@ public class Program {
 		System.out.print("Digite seu CPF: ");
 		int cpf = read.nextInt();
 		
-	    Cliente cliente = new Cliente(name, idade, cpf);
+	    cliente = new Cliente(name, idade, cpf);
 		
 		System.out.println("****CADASTRO FINALIZADO****");
 		System.out.println("Escolha seu tipo de conta: ");
@@ -36,16 +41,25 @@ public class Program {
 		
 		switch(tipoConta) {
 			case 1:
+				
 				System.out.println("Sua agencia vai ser 1.");
 				System.out.print("Escolha qual será sua conta: ");
 				conta = read.nextInt();
 				System.out.println("Dejesa fazer um deposito inicial?");
 				numero = read.nextInt();
+				
 				if(numero == 1) {
 					System.out.println("Qual o valor do deposito?");
 					double valorDeposito = read.nextDouble();
 					
-					Conta contaCorrente = new ContaCorrente(conta, valorDeposito);
+					acc = new ContaCorrente(conta, valorDeposito);
+					
+					//SUBROTINA OPERAÇÃO
+				}
+				else {
+					acc = new ContaCorrente(conta);
+					//SUBROTINA OPERAÇÃO
+					
 				}
 				break;
 			case 2:
@@ -53,16 +67,18 @@ public class Program {
 				System.out.println("Sua agencia vai ser 2.");
 				System.out.print("Escolha qual será sua conta: ");
 				conta = read.nextInt();
-				System.out.println("Dejesa fazer um deposito inicial?");
 				System.out.println("Qual o valor do deposito?");
 				double valorDeposito = read.nextDouble();
 					
-				Conta contaPoupança = new ContaCorrente(conta, valorDeposito);
+				acp = new ContaPoupanca(conta, valorDeposito);
+				
+				//SUBROTINA OPERAÇÃO
 				break;
 			default:
 				System.out.println("Número invalido!.");
 		}
 		
+		//COMEÇO DA SUBROTINA
 		System.out.println("Deseja fazer alguma operação?");
 		System.out.println("1 - DEPOSITO \n 2 - SAQUE \n 3 - TRANSFERENCIA");
 		int operacao = read.nextInt();
@@ -85,7 +101,7 @@ public class Program {
 			default:
 				System.out.println("Valor incorreto!.");
 		}
-		
+		//FINAL DA SUBROTINA
 		
 		read.close();
 	}
